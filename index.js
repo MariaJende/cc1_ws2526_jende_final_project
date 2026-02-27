3aa6e24c19fb835f75cf63d6a060fb4de1b8f443
MariaJende <maria.jende@t-online.de>
2026-02-27 11:24:43 +0100

    edit: vercel error

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ Used Claude.ai and Stack Overflow for inspiration, debugging and explanations
 
 //import * as THREE from "three";
 import {..} from './three.js'
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { HDRLoader } from "three/addons/loaders/HDRLoader.js";
+import { OrbitControls } from "./three/examples/jsm/controls/OrbitControls.js";
+import { HDRLoader } from "./three/addons/loaders/HDRLoader.js";
 
 
 // -------------------------------------
